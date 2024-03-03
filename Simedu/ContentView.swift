758c0105dec3a5543433bba87059e78b9a11f4ae
Jeremiah Oluwaseun Erinola <seunjeremiah@gmail.com>
2024-03-03 18:41:35 +0100

    More additions

--- Simedu/ContentView.swift
+++ Simedu/ContentView.swift
@@ -9,11 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 10) {
+            Text("Jeremiah Erinola").font(.system(size: 45, weight: .heavy, design: .serif)).multilineTextAlignment(.center).foregroundStyle(.black).padding(.bottom, 20)
+            Image(systemName: "globe").resizable().frame(width: 150, height: 150, alignment: .center).imageScale(.large)
+                .foregroundStyle(.green)
+                
+            Text("Hello, world!").padding(.bottom, 5)
             Text("This is my first iOS app, fully built on Swift Programming Language").bold().multilineTextAlignment(.center)
         }
         .padding()
